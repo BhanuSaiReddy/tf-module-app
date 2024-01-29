3c9cd8ebec06582049ac64ac3d8764e337dc06b6
bhanu <bhanusaireddy5244@gmail.com>
2024-01-29 16:19:24 +0530

    terraform.

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U  https://github.com/BhanuSaiReddy/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
+ansible-pull -i localhost, -U  https://github.com/BhanuSaiReddy/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
 

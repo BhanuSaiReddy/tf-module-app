8fc684b8bf0776e7285da8111c4e8328d2bdde3f
bhanu <bhanusaireddy5244@gmail.com>
2024-01-29 15:37:46 +0530

    terraform.

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
-#pip3.11 install botocore boto3 &>>/opt/userdata.log
+
 ansible-pull -i localhost, -U https://github.com/BhanuSaiReddy/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
 echo "Installation and configuration of APPS completed successfully."
 

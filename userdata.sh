69310699422f1ca57c2d45f22dd6c561b91ffdfb
bhanu <bhanusaireddy5244@gmail.com>
2024-01-29 16:51:37 +0530

    terraform.

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U  https://github.com/BhanuSaiReddy/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+yum install ansible -y &>>/opt/userdata.log
 
+ansible-pull -i localhost, -U https://github.com/BhanuSaiReddy/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+echo "Installation and configuration of APPS completed successfully."

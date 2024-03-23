6e518a3601ae9b6054a37abf9bff67d4698055c6
bhanu <bhanusaireddy5244@gmail.com>
2024-03-23 20:06:30 +0530

    terraform.io

--- main.tf
+++ main.tf
@@ -177,14 +177,25 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-  count = var.component == "frontend" ? 1 : 0
-  name     = "${local.name_prefix}-public"
-  port     = var.port
+  count       = var.component == "frontend" ? 1 : 0
+  name        = "${local.name_prefix}-public"
+  port        = var.port
   target_type = "ip"
-  protocol = "HTTP"
-  vpc_id   = var.default_vpc_id
-}
+  protocol    = "HTTP"
+  vpc_id      = var.default_vpc_id
 
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/"
+    port                = var.port
+    timeout             = 2
+    unhealthy_threshold = 2
+    matcher             = "404"
+  }
+
+}
 
 resource "aws_lb_target_group_attachment" "public" {
   count             = var.component == "frontend" ? length(var.subnet_ids) : 0
